7c72cb631f31d27738419d62253fb7185885a0a3
yukihana1220 <yukinko1220take@gmail.com>
2024-11-13 17:16:07 +0900

    Add files via upload
    課題005を提出

---
+++ kadai_005/score.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>テストの平均点を計算しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+     $score_1 = 80 ;
+     $score_2 = 60 ;
+     $score_3 = 55 ;
+     $score_4 = 40 ;
+     $score_5 = 100 ;
+     $score_6 = 25 ;
+     $score_7 = 80 ;
+     $score_8 = 95 ;
+     $score_9 = 30 ;
+     $score_10 = 60 ;
+     echo '<br>';
+
+     echo ($score_1 + $score_2 + $score_3 + $score_4 + $score_5 
+     + $score_6 + $score_7 + $score_8 + $score_9 + $score_10)/10;
+    ?>
+  </p>
+</body>
+
+
