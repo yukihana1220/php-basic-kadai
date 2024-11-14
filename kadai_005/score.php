f3bcd54da7bf1c42a994cd84ec7307d47e25be86
yukihana1220 <yukinko1220take@gmail.com>
2024-11-14 11:42:54 +0900

    課題005を再提出

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -19,10 +19,14 @@
      $score_8 = 95 ;
      $score_9 = 30 ;
      $score_10 = 60 ;
+     
+     $total_score = ($score_1 + $score_2 + $score_3 + $score_4 + $score_5 
+     + $score_6 + $score_7 + $score_8 + $score_9 + $score_10);
+
      echo '<br>';
 
-     echo ($score_1 + $score_2 + $score_3 + $score_4 + $score_5 
-     + $score_6 + $score_7 + $score_8 + $score_9 + $score_10)/10;
+     echo $total_score / 10;
+
     ?>
   </p>
 </body>
